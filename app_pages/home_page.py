7a2e9a5f50b6265e9fe32bc3e97aca866524dcc9
Joe Howley <joe.howley88@gmail.com>
2025-01-17 15:57:32 +0000

    add text content to summary page and link data set key

--- app_pages/home_page.py
+++ app_pages/home_page.py
@@ -9,7 +9,58 @@ import matplotlib.pyplot as plt
 def app():
     st.title("Football betting notebook")
 
-    st.write("Data driven betting predictions")
+    st.image("./images/premlogo.png")
+
+    st.header("Project Summary")
+
+    st.subheader("**Objective**")
+    st.write(
+        """
+        The Betting Tips Predictor Jupyter Notebook project is a predictive 
+        analytics tool designed to analyze historical sports data and provide 
+        informed betting recommendations. By leveraging machine learning and 
+        statistical models, the project aims to empower users with data-driven 
+        insights to enhance decision-making in the sports betting industry.
+        This initiative offers a cost-effective and scalable solution for both 
+        casual bettors and organizations looking to optimize their betting 
+        strategies.
+        """
+    )
+
+    st.subheader("Business Requirements")
+
+    st.write(
+        """
+        - Develop a Jupyter Notebook with a modular structure for easy 
+          scalability.
+        - Implement predictive models for win/loss probabilities and score 
+          forecasting.
+        - Incorporate data visualization tools for insightful and actionable 
+          outputs.
+        - Ensure reproducibility and transparency in the analytical process.
+        - Deliver a user-friendly interface to cater to both technical and 
+          non-technical users.
+
+        """
+    )
+
+    st.header("Data Set Summary")
+    st.write("The link for the dataset key is located beneath.")
+    st.page_link("https://www.football-data.co.uk/notes.txt", label="Dataset Key", icon="⚽")
+    st.write(
+        """
+        The data set is compiled from football statistics collected from over 
+        the last few years. they consist of various metrics, including:
+        - Goals scored
+        - Goals conceded
+        - Corners for, and corners against
+        - Fouls for and against
+        - Booking points
+
+        The list is extensive and there are many more. See more information and 
+        see the abbreviations used broken down and explained in the above link. 
+        """
+    )
     
     # Load the dataset
     file_path = "./data/full_dataset.csv"  # Replace with your dataset path
